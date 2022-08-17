2790f1f6d37cf577585f4bce9fac7421a013fdf9
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-08-16 22:37:10 -0300

    Declaração de variáveis e suas características

---
+++ Variable-Declaration.js
@@ -0,0 +1,14 @@
+// Pode ser resignado.
+// Pode ser redeclarado.
+// É armazenado no escopo global.
+// Hoisting (move as declarações de variáveis e de funções para o início do script).
+// Declara sem ser inicializado.
+var x = 2;
+
+// Pode ser resignado.
+// Tem escopo de bloco.
+// Declara sem ser inicializado.
+let y = 4;
+
+// Tem escopo de bloco.
+const z = 6;
