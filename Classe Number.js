85645a22b5aab3e9c7083af73a0e51d25f7556c0
Jonathan Mendes <89454975+DevJonathanMendes@users.noreply.github.com>
2022-09-01 07:15:17 -0300

    Add files via upload

---
+++ Classe Number.js
@@ -0,0 +1,31 @@
+// Conversão de números.
+var n = 123456.789;
+n.toFixed(0);       // "123457"
+n.toFixed(2);       // "123456.79"
+n.toFixed(5);       // "123456.78900"
+n.toExponential(1); // "1.2e+5"
+n.toExponential(3); // "1.235e+5"
+n.toPrecision(4);   // "1.235e+5"
+n.toPrecision(7);   // "123456.8"
+n.toPrecision(10);  // "123456.7890"
+
+// Se uma string é passada para a função de conversão Number(),
+// ela tenta analisar essa string como um inteiro ou literal em ponto flutuante.
+parseInt("3 blind mice")   // = 3
+parseFloat(" 3.14 meters") // = 3.14
+parseInt("-12.34")         // = -12
+parseInt("0xFF")           // = 255
+parseInt("0xff")           // = 255
+parseInt("-0XFF")          // = -255
+parseFloat(".1")           // = 0.1
+parseInt("0.1")            // = 0
+parseInt(".1")             // = NaN: inteiros não podem começar com "."
+parseFloat("$72.47");      // = NaN: números não podem começar com "$"
+
+// parseInt() aceita um segundo argumento opcional especificando a raiz (base) do número a ser analisado.
+// Os valores válidos estão entre 2 e 36.
+parseInt("11", 2);   // => 3 (1*2 + 1)
+parseInt("ff", 16);  // => 255 (15*16 + 15)
+parseInt("zz", 36);  // => 1295 (35*36 + 35)
+parseInt("077", 8);  // => 63 (7*8 + 7)
+parseInt("077", 10); // => 77 (7*10 + 7)
