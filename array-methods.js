2740fff556d67b1c6cd4a6fc89de033182d4fa1b
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-09-11 08:43:48 -0300

    modified

--- array-methods.js
+++ array-methods.js
@@ -1,8 +1,42 @@
-const array = [2, 4];
+let array = [1, 2, 3];
 
-// O método Array.join() converte todos os elementos de um array em strings e as concatena, retornando
-// a string resultante.
-array.join(" ") // "2 4"
+// Converte todos os elementos em strings, concatena e retorna.
+array.join(" ") // > "1 2 3"
+array.join(", ") // > "1, 2, 3"
 
-// O método Array.reverse() inverte a ordem dos elementos de um array e retorna o array invertido.
+// Inverte a ordem dos elementos e retorna o array invertido.
 array.reverse();
+
+// Retorna o array classificado. Quando chamado sem argumentos, classifica os elementos em ordem alfabética.
+array.sort(); // (Use callback para fazer classificações minuciosas)
+
+// Cria e retorna um novo array contendo os elementos do array original.
+array.concat(4, 5) // > [1, 2, 3, 4, 5]
+array.concat([4, 5]); // > [1, 2, 3, 4, 5]
+array.concat([4, 5], [6, 7]) // > [1, 2, 3, 4, 5, 6, 7]
+array.concat(4, [5, [6, 7]]) // > [1, 2, 3, 4, 5, [6, 7]]
+
+// Retorna um pedaço (ou subarray) do array especificado. Seus dois argumentos especificam o início e o fim do trecho a ser retornado.
+array.slice(0, 1) // > [1]
+array.slice(-1) // > [3]
+
+// push() e pop() permitem trabalhar com arrays como se fossem pilhas.
+// O método push() anexa um ou mais novos elementos no final e retorna o novo comprimento.
+// O método pop() faz o inverso: exclui o último elemento, decrementa o comprimento e retorna o valor que removeu.
+let stack = []; // > []
+stack.push(1, 2); // > [1, 2] Retorna o tamanho do array: 2
+stack.pop(); // > [1] Retorna o valor removido: 2
+stack.push(3); // > [1, 3] Retorna o tamanho do array: 2
+
+// unshift() e shift() se comportam quase como push() e pop(), exceto que inserem e removem elementos do início de um array e não do final.
+stack = []; // > []
+stack.unshift(1); // > [1] Retorna o tamanho do array: 1
+stack.unshift(22); // > [22,1] Retorna o tamanho do array: 2
+stack.shift(); // > [1] Retorna o valor removido: 22
+stack.unshift(3, [4, 5]); // > a:[3,[4,5],1] Retorna 3
+stack.shift(); // > [[4,5],1] Retorna o valor removido: 3
+
+// toString() e toLocaleString()
+stack = [1, 2, 3]
+stack.toString() // > "1,2,3"
+stack.toLocaleString() // > "1,2,3" Versão específica para a localidade.
