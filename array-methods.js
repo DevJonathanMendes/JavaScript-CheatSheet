14d1ed1c5856bc29d7988cc7f546af8844b97a35
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-09-10 23:38:37 -0300

    add

---
+++ array-methods.js
@@ -0,0 +1,8 @@
+const array = [2, 4];
+
+// O método Array.join() converte todos os elementos de um array em strings e as concatena, retornando
+// a string resultante.
+array.join(" ") // "2 4"
+
+// O método Array.reverse() inverte a ordem dos elementos de um array e retorna o array invertido.
+array.reverse();
