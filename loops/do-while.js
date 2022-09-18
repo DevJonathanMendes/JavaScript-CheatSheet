a92b901168c3af8817f3c1b32c8e1b623f01ff49
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-09-18 19:57:15 -0300

    modified

---
+++ loops/do-while.js
@@ -0,0 +1,9 @@
+// do-while sempre executa o corpo pelo menos uma vez antes de avaliar a expressão.
+
+let count = 0
+
+// Vai executar pelo menos uma vez.
+do {
+    count++;
+    console.log("count: ", count);
+} while (count < 3);
