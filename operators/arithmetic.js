5733d97eb48ca02d4f12e6c48eba359cdedf46b6
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-09-27 11:06:05 -0300

    modified

---
+++ operators/arithmetic.js
@@ -0,0 +1,25 @@
+// Os operadores aritméticos:
+2 + 2  // Adição.
+2 - 2  // Subtração.
+2 * 2  // Multiplicação.
+2 / 2  // Divisão.
+2 % 2  // Resto da divisão.
+2 ** 2 // Potência.
+2 + "2" // Saída: "22". "+" soma números, ou concatena strings.
+points[1].x - points[0].x // = 1: Operandos mais complicados também funcionam.
+
+
+// Operadores aritméticos de forma abreviada:
+var count = 0; // Variável para ilustrar.
+
+// Incremento:
+++count; // Pré-incremento.
+count++; // Pós-incremento.
+
+// Decremento:
+--count; // Pré-decremento.
+count--; // Pós-decremento.
+
+// Operador de atribuição é um atalho:
+count += 2;    // O mesmo que count = count + 2.
+count *= 2;    // O mesmo que count = count * 2.
