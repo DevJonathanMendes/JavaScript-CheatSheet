96d38719ec8f5531e6bf0661b5c15c2293bd2a84
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-09-28 10:45:29 -0300

    modified

--- operators/operators.js
+++ operators/operators.js
@@ -3,13 +3,11 @@
 // Os operadores de igualdade e relacionais testam se dois valores são iguais,
 // desiguais, menores que, maiores que, etc. São avaliados como verdadeiros ou falsos.
 var x = 2, y = 3; // Esses sinais = são atribuições e não testes de igualdade.
-x == y            // falso: igualdade
-x != y            // verdadeiro: desigualdade
+
 x < y             // verdadeiro: menor que
 x <= y            // verdadeiro: menor ou igual a
 x > y             // falso: maior que
 x >= y            // falso: maior ou igual a
-"two" == "three"  // falso: as duas strings são diferentes
 "two" > "three"   // verdadeiro: "tw" é alfabeticamente maior do que "th"
 false == (x > y)  // verdadeiro: falso é igual a falso
 
