543827a667e71757c81afb240e51646a338fdde2
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-10-01 14:35:59 -0300

    modified

--- Global-Objects/constructor-Object.js
+++ Global-Objects/constructor-Object.js
@@ -1,1 +1,16 @@
+const myObject = {
+    "property": "value",
+    method() {
+        console.log("Hello!");
+    }
+};
+
 Object();
+Object.create(); // É usado para criar um novo objeto e conectá-lo ao protótipo de um objeto existente.
+Object.keys(); // Cria um array que contém as chaves de um objeto.
+Object.values(); // Cria uma array que contém os valores de um objeto.
+Object.entries(); // Cria um array aninhado dos pares chave/valor de um objeto.
+Object.assign(); // É usado para copiar valores de um objeto para outro.
+Object.freeze(); // Impede a modificação de propriedades e valores, impede que as propriedades sejam adicionadas ou removidas de um objeto.
+Object.seal(); // Impede que novas propriedades sejam adicionadas a um objeto, mas permite a modificação de propriedades existentes.
+Object.getPrototypeOf(); // É usado para obter o [[Prototype]] interno oculto de um objeto, também acessível através da propriedade __proto__.
