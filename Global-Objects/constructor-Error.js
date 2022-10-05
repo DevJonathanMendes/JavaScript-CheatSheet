10e29663730aca29228b1f2befbfe6d0dee2c0c8
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-10-05 19:27:24 -0300

    modified

--- Global-Objects/constructor-Error.js
+++ Global-Objects/constructor-Error.js
@@ -1,1 +1,23 @@
+// O construtor Error() cria um objeto de erro.
+
+const message = "Uma descrição legível do erro.";
+
+// Não padrão, melhor verificar a compatibilidade.
+const fileName = "Propriedade fileName no objeto Error criado. Padrão é o nome do arquivo que contém o código que chamou o constructor Error().";
+
+// Não padrão, melhor verificar a compatibilidade.
+const lineNumber = "Propriedade lineNumber no objeto Error criado. padrão é o número da linha que contém a invocação do construtor Error().";
+
+const options = { cause: err };
+
+new Error();
+new Error(message);
+new Error(message, options);
+new Error(message, fileName);
+new Error(message, fileName, lineNumber);
+
 Error();
+Error(message);
+Error(message, options);
+Error(message, fileName);
+Error(message, fileName, lineNumber);
